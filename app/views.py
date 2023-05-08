04fb9783f3e2936edc4f55552fcab32270940fe4
MEKALAVANI123 <vanimekala4@gmail.com>
2023-05-08 21:26:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from app.forms import *
+from django.core.mail import send_mail
+
+def registration(request):
+    ufo=UserForm()
+    pfo=ProfileForm()
+    d={'ufo':ufo,'pfo':pfo}
+
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            NSUO=ufd.save(commit=False)
+            password=ufd.cleaned_data['password']
+            NSUO.set_password(password)
+            NSUO.save()
+            NSPO=pfd.save(commit=False)
+            NSPO.username=NSUO
+            NSPO.save()
+
+            send_mail('Registration',
+                      "Successfully Registration is Done",
+                      'vanivasavi99123@gmail.com',
+                      [NSUO.email],
+                      fail_silently=False
+                      )
+                      
+            return HttpResponse('Registration is successfully')
+        else:
+            return HttpResponse('Not valid')
+    return render(request,'registration.html',d)
